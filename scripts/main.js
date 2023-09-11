e3bc7ecc0dde79d41f8f6f84adf4165e246e1523
Mehwish <mehw337@gmail.com>
2023-09-11 09:25:38 +0500

    PRACTISE 1

---
+++ scripts/main.js
@@ -0,0 +1,62 @@
+const myHeading = document.querySelector("h2");
+myHeading.textContent = "Practise No 1";
+
+
+// Variable Declaration and Assignment:
+let Num1,Num2,result;
+Num1=10;
+Num2=20;
+// Addition
+result=Num1+Num2;
+console.log("Result=",result);
+// Subtraction
+console.log(" Subtraction Result=",result);
+result=Num1-Num2;
+// Multiplication
+console.log(" Multiplication Result=",result);
+result=Num1*Num2;
+console.log("Division Result=",result);
+// Division
+result=Num1/Num2;
+
+
+// Data Types and Type Conversion:
+let text1="Bonzer";
+let text2=" Point";
+let Istrue= true;
+
+result=text1+text2;
+console.log("Concatenation result=",result);
+if(Num1>Num1)
+{
+    console.log("num1",Num1)
+}
+else
+{
+    console.log("num2",Num2)
+}
+
+if(text1==" Bonze")
+{
+    console.log("true")
+}
+else{
+    console.log("false")
+}
+
+if(text1&&text2=="Bonzer")
+{
+    console.log('True')
+}
+else{
+    console.log('False')
+}
+
+
+if(text1||text2=="Bonzer")
+{
+    console.log('True')
+}
+else{
+    console.log('False')
+}
